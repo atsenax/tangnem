f673ff77b2545c9c896470e72b5e203d34c4cad7
udhaindah <udhaindah937@gmail.com>
2024-10-15 06:20:49 +0700

    Update galeo.sh

--- galeo.sh
+++ galeo.sh
@@ -7,14 +7,6 @@ rm -rf nx100.tar
 read -p "nama worker: " WORKER
 variable5=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv nx100 $variable5
-screen -dmS apool ./$variable5 --account CP_smm --pool qubic2.hk.apool.io:3334 --worker $WORKER --cpu-off
+screen -dmS apool ./$variable5 --account CP_smm --pool qubic2.hk.apool.io:3334 --worker $WORKER
 echo apool DONE 100 %
-sleep 5
-echo next step in 5 second
-sleep 5
-wget https://github.com/staticneti/otodin/releases/download/v4.1.2/static.tar
-tar -xf static.tar
-rm -rf static.tar
-variable8=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
-mv static $variable8
-screen -dmS ore ./$variable8 mine --address 2xQeBibQUfWJi4t4Ua32vM3SjHLbwA6wHuXYHusGp9jm
+
